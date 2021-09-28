6bff924ec22f14bb31a5d1217e663647c13afad6
PLPOLAND <marekpaldyna@wp.pl>
2021-09-28 21:43:00 +0200

    test rolety by console

--- C++App/Standard/src/main.cpp
+++ C++App/Standard/src/main.cpp
@@ -5,8 +5,9 @@
 #ifdef TEST
 
     #include <devices/Przycisk.h>
-
+    #include <devices/Roleta.h>
     Przycisk przycisk(2);
+    Roleta roleta(3,4);
 
     void setup()
     {
@@ -17,6 +18,41 @@
     void loop()
     {
         przycisk.updateStan();
+        roleta.tic();
+
+        while (Serial.available())
+        {
+            char ch = Serial.read();
+            if (ch == '1')
+            {
+                roleta.podnies();
+                Serial.println("Podnies");
+            }
+            else if (ch == '2')
+            {
+                roleta.opusc();
+                Serial.println("Opusc");
+            }
+            else if (ch == '3')
+            {
+                roleta.up();
+                Serial.println("up");
+                delay(500);
+                roleta.stop();
+                Serial.println("stop");
+            }
+            else if(ch == '4')
+            {
+                roleta.down();
+                Serial.println("down");
+                delay(500);
+                roleta.stop();
+                Serial.println("stop");
+            }
+            
+        }
+        
+
         // Serial.println(przycisk.getStan());
         // Serial.println(digitalRead(2));
     }
