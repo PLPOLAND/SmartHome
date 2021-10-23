fca093e6778a15492b1526bd06cbc14923c872a0
PLPOLAND <marekpaldyna@wp.pl>
2021-10-23 21:53:55 +0200

    Porządek w plikach klasy

--- C++App/Standard/src/devices/Przekaznik.cpp
+++ C++App/Standard/src/devices/Przekaznik.cpp
@@ -23,3 +23,38 @@ Przekaznik::Przekaznik(byte pin, bool stan = false)
         digitalWrite(pin, LOW);
     }
 }
+
+byte Przekaznik::getPin()
+{
+    return pin;
+};
+
+//Ustawianie pinu uzywanego do sterowania przekaźnikiem
+void Przekaznik::setPin(byte pin)
+{
+    if (pin >= 2 && pin <= 16)
+    {
+        digitalWrite(pin, LOW); //resetowanie poprzedniego stanu wyjscia
+        this->pin = pin;
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, stan == false ? LOW : HIGH);
+    }
+};
+
+bool Przekaznik::getStan()
+{
+    return stan;
+};
+//włącz/wyłącz przekaźnik (true/false)
+void Przekaznik::setStan(bool stan)
+{
+    this->stan = stan;
+    digitalWrite(pin, stan == false ? LOW : HIGH);
+};
+//włącz/wyłącz przekaźnik (1 = włącz; inna wartość = wyłącz)
+void Przekaznik::setStan(int stan)
+{
+    this->stan = stan == 1 ? true : false;
+
+    digitalWrite(pin, this->stan == false ? LOW : HIGH);
+}
--- C++App/Standard/src/devices/Przekaznik.h
+++ C++App/Standard/src/devices/Przekaznik.h
@@ -14,39 +14,12 @@ public:
     Przekaznik();
     Przekaznik(byte pin, bool stan);
     ~Przekaznik();
-    byte getPin()
-    {
-        return pin;
-    };
-
-    //Ustawianie pinu uzywanego do sterowania przekaźnikiem
-    void setPin(byte pin)
-    {
-        if (pin >= 2 && pin <= 16)
-        {
-            digitalWrite(pin, LOW); //resetowanie poprzedniego stanu wyjscia
-            this->pin = pin;
-            pinMode(pin, OUTPUT);
-            digitalWrite(pin, stan == false ? LOW : HIGH);
-        }      
-    };
-
-    bool getStan(){
-        return stan;
-    };
-    //włącz/wyłącz przekaźnik (true/false)
-    void setStan(bool stan)
-    {
-        this->stan = stan;
-        digitalWrite(pin, stan == false ? LOW : HIGH);
-    };
-    //włącz/wyłącz przekaźnik (1 = włącz; inna wartość = wyłącz)
-    void setStan(int stan)
-    {
-        this->stan = stan == 1 ? true : false;
-
-        digitalWrite(pin, this->stan == false ? LOW : HIGH);
-    }
+    
+    byte getPin();
+    void setPin(byte pin); 
+    bool getStan();
+    void setStan(bool stan);
+    void setStan(int stan);
 };
 
 #endif // !Przekaznik_h
