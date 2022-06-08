5ccccf5d99702a4973298f96ba68b19ee404e7be
PLPOLAND <marekpaldyna@wp.pl>
2022-06-08 16:39:27 +0200

    dodanie pokoju na liście sensorów

--- javaApp/SmartHome/src/main/resources/static/js/admin/pages/sensorList.js
+++ javaApp/SmartHome/src/main/resources/static/js/admin/pages/sensorList.js
@@ -1,4 +1,25 @@
+var rooms;
 $(document).ready(function () {
+
+    $.ajax({
+        url: "api/getRoomsNamesList",
+        type: 'get',
+        data: {
+        },
+        success: function (response) {
+            // console.log(response);
+
+            // $("#err-msg").html(response);
+            if (response.error == null) {
+                console.log(response.obj);
+                rooms = response.obj;
+            } else {
+                $("#err-msg").html(response.error);
+                $("#err-msg").show("bounce", {}, 1000, function () { hideAfter(this, 10000) });
+            }
+        }
+    });
+
     $.ajax({
         url: "/admin/api/getSensors",
         type: 'post',
@@ -9,7 +30,7 @@ $(document).ready(function () {
                 var list = $(".list");
                 response.obj.sort(compareSensors)
                 response.obj.forEach(element => {
-                    list.append(makeFullSensor(element));
+                    list.append(makeFullSensor(element,rooms));
                 });
             } else {
                 $("#err-msg").html(response.error);
