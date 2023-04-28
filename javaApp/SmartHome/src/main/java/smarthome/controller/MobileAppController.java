bbc98d6d97e66b55884e519cce603face8e600cf
Marek Pałdyna <marekpaldyna@wp.pl>
2023-04-28 19:26:13 +0200

    Login done

--- javaApp/SmartHome/src/main/java/smarthome/controller/MobileAppController.java
+++ javaApp/SmartHome/src/main/java/smarthome/controller/MobileAppController.java
@@ -2,6 +2,9 @@ package smarthome.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import smarthome.database.UsersDAO;
+import smarthome.security.MobileSecurity;
 import smarthome.security.Security;
+import smarthome.model.Response;
+import smarthome.model.user.User;
 
 @RestController
 @RequestMapping("/api")
@@ -20,17 +26,38 @@ public class MobileAppController {
 	@Autowired
 	UsersDAO users;
 
-	// @RequestMapping("/")
-	// public String mainpage(HttpServletRequest request){
-	// 	Security sec = new Security(request, users);
-	// 	if(!sec.isLoged())
-	// 		return "redirect:login";
+	Logger logger = LoggerFactory.getLogger(MobileAppController.class);//logger
 
-	// 	return "index";
-	// }
-	@GetMapping("/hello")
+	/** 
+	 * For serching server from mobile app
+	 */
+	@GetMapping("/homeData")
 	public String hello() {
 		return "hello";
+	}
+
+	@PostMapping("/login")
+	public Response<String> login(HttpServletRequest request) {
+		MobileSecurity security = new MobileSecurity(request, users);
+		String token = security.login();
+		if (token == null || token.isEmpty()) {
+			return new Response<>("", "Niepoprawne dane logowania");
+		} else {
+			return new Response<>("{\"token\": \""+ token + "\"}");
+		}
+	}
+	@PostMapping("/getUserData")
+	public Response<String> getUserData(HttpServletRequest request) {
+		MobileSecurity security = new MobileSecurity(request, users);
+		if(!security.isLoged() )
+			return new Response<>("", "Użytkownik nie jest zalogowany");
+
+		User user = security.getFullUserData();
+		if (user == null) {
+			return new Response<>("", "Nie znaleziono użytkownika, błąd wewnętrzny!");
+		} else {
+			return new Response<>(user.toString());
+		}
 	}
 
 }
