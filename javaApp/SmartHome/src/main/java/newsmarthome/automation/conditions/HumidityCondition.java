7ea813c2692ecf1430f74aa3ab1995d4af45153b
Marek Pałdyna <marekpaldyna@wp.pl>
2023-10-19 12:29:29 +0200

    feat: doc bugfix: fix to constructors with 2 values

--- javaApp/SmartHome/src/main/java/newsmarthome/automation/conditions/HumidityCondition.java
+++ javaApp/SmartHome/src/main/java/newsmarthome/automation/conditions/HumidityCondition.java
@@ -2,14 +2,37 @@ package newsmarthome.automation.conditions;
 
 import newsmarthome.model.hardware.sensor.Higrometr;
 
+/**
+ * A condition that checks the humidity level of a Higrometr sensor.
+ * The condition can check if the humidity level is equal, not equal, greater, less, greater or equal, less or equal, or within a range of values.
+ * @see SensorCondition
+ */
 public class HumidityCondition extends SensorCondition<Integer>{
+
+    /**
+     * Constructs a new HumidityCondition object with a Higrometr sensor, a value check type, and a value.
+     * @param sensor the Higrometr sensor to check the humidity level of
+     * @param valueCheckType the type of value check to perform (equal, not equal, greater, less, greater or equal, less or equal, or within a range of values)
+     * @param value the value to check against the humidity level of the sensor
+     */
     public HumidityCondition(Higrometr sensor,ValueCheckType valueCheckType, Integer value) {
         super(sensor, valueCheckType, value);
     }
 
-    public HumidityCondition(Higrometr sensor, ValueCheckType valueCheckType,Integer value, Integer value2) {
-        super(sensor, valueCheckType, value, value2);
+    /**
+     * Constructs a new HumidityCondition object with a Higrometr sensor, a minimum value, and a maximum value.
+     * @param sensor the Higrometr sensor to check the humidity level of
+     * @param value the minimum value to check against the humidity level of the sensor
+     * @param value2 the maximum value to check against the humidity level of the sensor
+     */
+    public HumidityCondition(Higrometr sensor, Integer value, Integer value2) {
+        super(sensor, value, value2);
     }
+
+    /**
+     * Checks if the humidity level of the sensor meets the condition.
+     * @return true if the humidity level meets the condition, false otherwise
+     */
     @Override
     public boolean checkCondition() {
         Integer sensorValue = ((Higrometr) sensor).getHumidity();
--- javaApp/SmartHome/src/main/java/newsmarthome/automation/conditions/TemperatureCondition.java
+++ javaApp/SmartHome/src/main/java/newsmarthome/automation/conditions/TemperatureCondition.java
@@ -5,20 +5,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import newsmarthome.model.hardware.sensor.Termometr;
 
+/**
+ * A condition that checks the temperature value of a thermometer sensor against a given value or range of values.
+ * Extends the SensorCondition class with a Float type parameter.
+ * @see SensorCondition
+ */
 public class TemperatureCondition extends SensorCondition<Float>{
     Logger logger;
 
+    /**
+     * Constructor for a TemperatureCondition object with a single value to check against.
+     * @param sensor The thermometer sensor to check the temperature value of.
+     * @param valueCheckType The type of check to perform (e.g. EQUAL, GREATER, LESS).
+     * @param value The value to check the temperature against.
+     */
     public TemperatureCondition(Termometr sensor, ValueCheckType valueCheckType, Float value) {
         super(sensor, valueCheckType, value);
         logger = LoggerFactory.getLogger(TemperatureCondition.class);
-        
     }
 
-    public TemperatureCondition(Termometr sensor, ValueCheckType valueCheckType, Float value, Float value2) {
-        super(sensor, valueCheckType, value, value2);
+    /**
+     * Constructor for a TemperatureCondition object with a range of values to check against.
+     * @param sensor The thermometer sensor to check the temperature value of.
+     * @param value The lower bound of the range to check the temperature against.
+     * @param value2 The upper bound of the range to check the temperature against.
+     */
+    public TemperatureCondition(Termometr sensor, Float value, Float value2) {
+        super(sensor, value, value2);
         LoggerFactory.getLogger(TemperatureCondition.class);
     }
 
+    /**
+     * Checks if the temperature value of the sensor satisfies the condition.
+     * @return True if the condition is satisfied, false otherwise.
+     */
     @Override
     public boolean checkCondition() {
         Float sensorValue = ((Termometr) sensor).getTemperatura();
